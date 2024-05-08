e2cedfea5b53e527761fb246eb0bd065b42d83f6
ahema-88 <143810732+ahema-88@users.noreply.github.com>
2024-05-08 01:54:44 +0000

    Update main.R

--- main.R
+++ main.R
@@ -2,13 +2,9 @@ library(tidyverse)
 library(here)
 
 tv_hours_table <- gss_cat %>%
-  filter(age<30) %>% 
+  filter(age>=30) %>% 
   group_by(marital) %>%
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 write_csv(tv_hours_table, here("Tv_hours_by_marital.csv"))
 
-
-# lien du github1== https://github.com/ahema-88/tv_hours_analysis
-
-# Practice 1
 
